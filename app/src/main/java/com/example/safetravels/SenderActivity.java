c08b752b2b2322dec17be41c63d8949c876f5b06
jdickson0296 <jdickson0296@gmail.com>
2019-10-17 12:34:49 -0700

    added select file button to screen

--- app/src/main/java/com/example/safetravels/SenderActivity.java
+++ app/src/main/java/com/example/safetravels/SenderActivity.java
@@ -2,7 +2,6 @@ package com.example.safetravels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
@@ -18,6 +17,7 @@ public class SenderActivity extends AppCompatActivity {
     EditText Stegan_key_text_S;
     Button Encrypt;
     Button upload;
+    Button select_s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,7 @@ public class SenderActivity extends AppCompatActivity {
         Stegan_key_text_S = (EditText) findViewById(R.id.stegan_key_s);
         Encrypt = (Button) findViewById(R.id.encrypt_button);
         upload = (Button) findViewById(R.id.upload_button);
+        select_s =(Button) findViewById(R.id.select_button_s);
 
 
         // get the aes key from the text box
@@ -39,11 +40,12 @@ public class SenderActivity extends AppCompatActivity {
         String stegan_key_s = String.valueOf(Stegan_key_text_S.getText());
 
 
-
         // function for when encrypt is clicked
         Encrypt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
 
             }
         });
